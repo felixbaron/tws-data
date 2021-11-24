a416b7ed0416549246c0cde225b97a305ccb290c
Felix Baron <45421716+felixbaron@users.noreply.github.com>
2021-11-24 11:42:45 +0100

    add Docker image for TWS

--- images/tws/run.sh
+++ images/tws/run.sh
@@ -2,7 +2,5 @@
 # Set up the X11 screen simulator
 Xvfb :1 -ac -screen 0 1920x1080x24 &
 x11vnc -ncache 10 -ncache_cr -passwd headless -display :1 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6
-# Start TWS Offline installation
-DISPLAY=:1 ./tws-latest-standalone-linux-x64.sh
 # Launch TWS Offline
 DISPLAY=:1 /root/Jts/1011/tws
